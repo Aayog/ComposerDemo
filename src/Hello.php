f20bf558baa3ea40928cf82f9f7131689aa67164
koirala <koirala@miamioh.edu>
2022-05-04 09:16:30 -0400

    init demo

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Aayog Koirala<koirala@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace koirala\Composerdemo;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
